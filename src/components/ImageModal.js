62c3b492896eaf08b00e00f766453f782bd6c07e
Melih Onvural <melih.onvural@gmail.com>
2019-10-12 00:41:52 -0400

    View bigger image with pop-up
    Add a big default image, and allow it to be viewed on click of the image on the page

---
+++ src/components/ImageModal.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import Modal from 'react-bootstrap/Modal';
+
+class ImageModal extends Component {
+  render() {
+    return (
+      <Modal
+        show={this.props.show}
+        onHide={this.props.onHide}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        >
+        <Modal.Body>
+          <img src={this.props.modalimageurl} alt="Large version of the homepage image" onClick={this.imagePopup}/>
+        </Modal.Body>
+      </Modal>
+    );
+  }
+}
+
+export default ImageModal;
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,23 @@ import React, {Component} from 'react';
 import TimelineItem from './TimelineItem.js';
 import DefaultImage from './DefaultImage.js';
 import DefaultBlogLink from './DefaultBlogLink.js';
+import ImageModal from './ImageModal.js';
 import todayData from './data.js';
 
 class Timeline extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {showBigImage: false, modalImageUrl: 'images/full/default.jpg'}
+    this.triggerImageModal = this.triggerImageModal.bind(this);
+  }
+
+  triggerImageModal = (modalImageUrl) => {
+    this.setState({
+      showBigImage: !this.state.showBigImage,
+      modalImageUrl: modalImageUrl
+    });
+  }
 
   render() {
     if(todayData.length > 0) {
@@ -13,13 +27,23 @@ class Timeline extends Component {
             {todayData.map((data, idx) => (
                 <TimelineItem data={data} key={idx} />
             ))}
+            <ImageModal
+              show={this.state.showBigImage}
+              onHide={this.triggerImageModal}
+              modalimageurl={this.state.modalImageUrl}
+            />
         </div>
       );
     } else {
       return (
         <div className="timeline-container">
-          <DefaultImage />
+          <DefaultImage onImageClick={this.triggerImageModal}/>
           <DefaultBlogLink />
+          <ImageModal
+            show={this.state.showBigImage}
+            onHide={this.triggerImageModal}
+            modalimageurl={this.state.modalImageUrl}
+          />
         </div>
       );
     }
