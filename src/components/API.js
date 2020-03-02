52c953ab5f3f8e85bd461253f11a49fe6d592175
Melih Onvural <melih.onvural@reconnect.io>
2020-03-01 22:23:57 -0500

    Update API.js

--- src/components/API.js
+++ src/components/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://onvural-website-backend.herokuapp.com/",
   responseType: "json"
 });
